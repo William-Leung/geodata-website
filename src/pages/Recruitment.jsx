3fdda1e031c3ba8711c52849eca91abf59495bc8
sophiacrosby61 <sec335@cornell.edu>
2025-02-28 17:45:09 -0500

    update recruitment

--- src/pages/Recruitment.jsx
+++ src/pages/Recruitment.jsx
@@ -8,6 +8,9 @@ export default function Recruitment(){
   return(
     <>
       <h1>recruitment :D</h1>
+      <p> Thank you for your interest in CU Geodata! We recently began our recruitment process for the Spring 2025 semester! Below you can find a link to our team application, which includes a set of short questions to learn more about you. We also hold coffee chats if you want to learn more about us!
+
+Please note that the General Project Team Application must be completed in addition to the CU GeoData Application. </p>
     </>
   )
 }
