b92dd203cd3cb4a428e079f77c492ff189c13976
sophiacrosby61 <sec335@cornell.edu>
2025-03-02 14:55:32 -0500

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,15 @@ export default function Home(){
   return(
     <>
       <h1>GeoData Homepage</h1>
+      <p> We perform ground-breaking research on local environments and ecosystems </p>
+
+      <p> As the Department of Earth and Atmospheric Science’s only student project team, we have the unique ability to leverage EAS resources and support combined with private funding to perform student-led research in the College of Engineering’s Project Team Program.</p>
+
+
+      <p> Our Organization </p>
+      <p> Find out about our mission, impact, and leadership structure. </p>
+
+
     </>
   )
 }
