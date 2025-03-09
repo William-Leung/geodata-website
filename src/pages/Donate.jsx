6f9050364220a1a3f256f4b10964010a965dfd56
William Leung <wcl53@cornell.edu>
2025-03-09 14:44:26 -0400

    Changing tab name dynamically

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -3,7 +3,9 @@ export default function Donate(){
 
   const[count, setCount] = useState(0)
 
-
+  useEffect(() => {
+    document.title = `Donate Page`;
+  });
 
   return(
     <>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ export default function Home(){
 
   const[count, setCount] = useState(0)
 
-
+  useEffect(() => {
+    document.title = `Home Page`;
+  });
 
   return(
     <>
