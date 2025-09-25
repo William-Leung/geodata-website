440f4f85c9dcc3c312e667a27c0686021f18aea8
Ashley Zhao <139811328+ash13e@users.noreply.github.com>
2025-09-25 17:11:13 -0400

    fixed minimum window/new members data file

--- src/components/Member Data.js
+++ src/components/Member Data.js
@@ -1,4 +1,31 @@
-const member = {name: "William Leung", role: "Data", img: "../assets/images/william.jpg", bio: "William is quite tired!",}
+import defaultPhoto from '../assets/images/default.jpg'
+import williamHeadshot from '../assets/images/william_leung_headshot.png'
+import orionHeadshot from '../assets/images/orion_hoch_headshot.jpeg'
 
+const teamMembers = [
+    {
+      id: 1,
+      name: 'William Leung',
+      role: 'Data Team Lead',
+      photo: williamHeadshot,
+      bio: 'I\'m William and I love to bake! So far, I\'ve made egg tarts, pineapple bread, tiramisu, and cheesecake but I\'m sure more will be added to the list soon :) Outside of baking, I like to run, daydream, and go to the Farmer\'s Market.'
+    },
+    {
+      id: 2,
+      name: 'Orion Hoch',
+      role: 'Water Team Lead',
+      photo: orionHeadshot,
+      bio: 'Hey I\'m Orion! Outside of the team you can find me on WVBR talking sports and movies, biking around Ithaca, and playing in 9 Fantasy leagues.'
+    },
+    {
+      id: 3,
+      name: 'Ashley Zhao',
+      role: 'Data Team Member',
+      photo: defaultPhoto,
+      bio: 'hi.'
+    }
+  ];
+
+export default teamMembers;
 
 
--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import MemberCard from '../components/MemberCard';
-import defaultPhoto from '../assets/images/default.jpg'
-import williamHeadshot from '../assets/images/william_leung_headshot.png'
-import orionHeadshot from '../assets/images/orion_hoch_headshot.jpeg'
+import teamMembers from '../components/Member Data'
 
 
 export default function Members() {
@@ -30,29 +28,6 @@ export default function Members() {
     };
   }, []);
 
-  const teamMembers = [
-    {
-      id: 1,
-      name: 'William Leung',
-      role: 'Data Team Lead',
-      photo: williamHeadshot,
-      bio: 'I\'m William and I love to bake! So far, I\'ve made egg tarts, pineapple bread, tiramisu, and cheesecake but I\'m sure more will be added to the list soon :) Outside of baking, I like to run, daydream, and go to the Farmer\'s Market.'
-    },
-    {
-      id: 2,
-      name: 'Orion Hoch',
-      role: 'Water Team Lead',
-      photo: orionHeadshot,
-      bio: 'Hey I\'m Orion! Outside of the team you can find me on WVBR talking sports and movies, biking around Ithaca, and playing in 9 Fantasy leagues.'
-    },
-    {
-      id: 3,
-      name: 'Ashley Zhao',
-      role: 'Data Team Member',
-      photo: defaultPhoto,
-      bio: 'hi.'
-    }
-  ];
 
   return (
     <div>
